8554e0974c16c86137384dd9e54d8f471dfa53ef
spas1isohrani <78902102+spas1isohrani@users.noreply.github.com>
2021-06-06 18:25:32 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -6,5 +6,26 @@
 #include  "bst.h"
 
 BST<std::string> makeTree(const char* filename) {
-  // поместите сюда свой код
+  BST<std::string> tree;
+  std::string str = "";
+  std::ifstream file(filename);
+  if (!file) {
+    std::cout « "Reading file error!" « std::endl;
+    return tree;
+  }
+  char diff = 'a' - 'A';
+  while (!file.eof()) {
+    char sym = file.get();
+    if (((sym >= 'a') && (sym <= 'z')) || ((sym >= 'A') && (sym <= 'Z'))) {
+      if ((sym >= 'A') && (sym <= 'Z')) {
+        sym += diff;
+      }
+      str += sym; }
+    else if (str != "") {
+      tree.add(str);
+      str = "";
+    }
+  }
+  file.close();
+  return tree;
 }
